d00ee505da62da8f1c4c8bf5bcb193bce0cc1a37
Ignas Paulionis <ignaspaulionis@ignasmacbookpro.localdomain>
2019-12-28 20:22:24 +0200

    first commit

---
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 24 16:28:48 2019
+
+@author: ignaspaulionis
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd  
+
+# importing dataset
+
